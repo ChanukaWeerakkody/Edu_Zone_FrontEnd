0a2ad57266627f52169ebb4363efc70f7ac9e72c
Chanuka <chanuka.weerakkody123@gmail.com>
2024-01-17 23:35:27 +0530

    finish create course information part

--- app/componenets/admin/course/CourseInformation.tsx
+++ app/componenets/admin/course/CourseInformation.tsx
@@ -16,6 +16,44 @@ const CourseInformation:FC<Props> = ({courseInfo,setCourseInfo,active,setActive}
         setActive(active + 1);
     };
 
+    const handleFileChange = (e:any) => {
+        const file = e.target.files?.[0];
+        if(file){
+            const reader = new FileReader();
+            reader.onload = (e:any) => {
+                if(reader.readyState === 2){
+                    setCourseInfo({...courseInfo, thumbnail: reader.result});
+                }
+            };
+            reader.readAsDataURL(file);
+        }
+    }
+
+    const handleDragOver = (e:any) => {
+        e.preventDefault();
+        setDragging(true);
+    }
+
+    const handleDragLeave = (e:any) => {
+        e.preventDefault();
+        setDragging(false);
+    }
+
+    const handleDrop = (e:any) => {
+        e.preventDefault();
+        setDragging(false);
+
+        const file = e.dataTransfer.files?.[0];
+        if(file){
+            const reader = new FileReader();
+            reader.onload = () => {
+                setCourseInfo({...courseInfo, thumbnail: reader.result});
+            };
+            reader.readAsDataURL(file);
+        }
+    }
+
+
     return (
         <div className="w-[80%] m-auto mt-24">
             <form onSubmit={handleSubmit} className={`${styles.label}`}>
@@ -52,6 +90,7 @@ const CourseInformation:FC<Props> = ({courseInfo,setCourseInfo,active,setActive}
                     </textarea>
                 </div>
                 <br/>
+
                 <div className="w-full flex justify-between">
                     <div className="w-[45%]">
                         <label className={`${styles.label}`}>Course Price</label>
@@ -68,8 +107,116 @@ const CourseInformation:FC<Props> = ({courseInfo,setCourseInfo,active,setActive}
                             className={`${styles.input}`}
                         />
                     </div>
-
+                    <div className="w-[50%]">
+                        <label className={`${styles.label}`}>Estimated Price(Optional)</label>
+                        <input
+                            type="number"
+                            name=""
+                            required
+                            value={courseInfo.estimatedPrice}
+                            onChange={(e:any) =>
+                                setCourseInfo({...courseInfo, estimatedPrice: e.target.value})
+                            }
+                            id="estimatedPrice"
+                            placeholder="120"
+                            className={`${styles.input}`}
+                        />
+                    </div>
                 </div>
+                <br/>
+                <div>
+                    <label className={`${styles.label}`} htmlFor="email">
+                        Course Tags
+                    </label>
+                    <input
+                        type="text"
+                        name=""
+                        required
+                        value={courseInfo.tags}
+                        onChange={(e:any) =>
+                            setCourseInfo({...courseInfo, tags: e.target.value})
+                        }
+                        id="tags"
+                        placeholder="Mern Stack"
+                        className={`${styles.input}`}
+                    />
+                </div>
+
+                {/*Space*/}
+                <br/>
+                <div className="w-full flex justify-between">
+                    <div className="w-[45%]">
+                        <label className={`${styles.label}`}>Course Level</label>
+                        <input
+                            type="text"
+                            name=""
+                            required
+                            value={courseInfo.level}
+                            onChange={(e:any) =>
+                                setCourseInfo({...courseInfo, level: e.target.value})
+                            }
+                            id="level"
+                            placeholder="Beginner/Intermediate/Expert"
+                            className={`${styles.input}`}
+                        />
+                    </div>
+                    <div className="w-[50%]">
+                        <label className={`${styles.label}`}>Demo Url</label>
+                        <input
+                            type="text"
+                            name=""
+                            required
+                            value={courseInfo.demoUrl}
+                            onChange={(e:any) =>
+                                setCourseInfo({...courseInfo, demoUrl: e.target.value})
+                            }
+                            id="demoUrl"
+                            placeholder="eeeee"
+                            className={`${styles.input}`}
+                        />
+                    </div>
+                </div>
+                <br/>
+
+                <div className="w-full">
+                    <input
+                        type="file"
+                        accept="image/*"
+                        id="file"
+                        className="hidden"
+                        onChange={handleFileChange}
+                    />
+                    <label htmlFor="file" className={`w-full min-h-[10vh] dark:border-white border-[#00000026] p-3 border flex items-center justify-center ${
+                        dragging ? "bg-blue-500" : "bg-transparent"
+                    }`}
+                    onDragOver={handleDragOver}
+                    onDragLeave={handleDragLeave}
+                    onDrop={handleDrop}
+                    >
+                        {
+                            courseInfo.thumbnail ? (
+                                <img src={courseInfo.thumbnail} alt="thumbnail" className=" max-h w-full object-cover"/>
+                            ) : (
+                               <span className="text-black dark:text-white">
+                                   Drag and drop your thumbnail here or click to select a file
+                               </span>
+                            )
+                        }
+                    </label>
+                </div>
+                <br/>
+                <div className="w-full flex items-center justify-end">
+                    <input
+                        type="submit"
+                        value="Next"
+                        className="w-full 800px:w-[180px] h-[40px] bg-[#37a391] text-center text-white rounded mt-8 cursor-pointer"
+                    />
+                </div>
+                <br/><br/>
+
+
+
+
             </form>
         </div>
     )
