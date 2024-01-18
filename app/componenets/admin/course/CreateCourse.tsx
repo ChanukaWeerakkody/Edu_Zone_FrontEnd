d69e02dc526ab0f599dae6912c02831528297487
Chanuka <chanuka.weerakkody123@gmail.com>
2024-01-18 23:48:48 +0530

    start to create course preview part in create course

--- app/componenets/admin/course/CreateCourse.tsx
+++ app/componenets/admin/course/CreateCourse.tsx
@@ -4,11 +4,12 @@ import CourseInformation from './CourseInformation';
 import CourseOptions from './CourseOptions';
 import CourseData from './CourseData';
 import CourseContent from './CourseContent';
+import CoursePreview from './CoursePreview';
 
 type Props = {};
 
 const CreateCourse = (props: Props) => {
-    const [active, setActive] = React.useState(2);
+    const [active, setActive] = React.useState(0);
     const [courseInfo, setCourseInfo] = useState({
         name: "",
         description: "",
@@ -39,8 +40,43 @@ const CreateCourse = (props: Props) => {
     const [courseData, setCourseData] = useState({});
 
     const handleSubmit = async () => {
+        //Format benefit array
+        const formattedBenefits = benefits.map((benefit) => ({title:benefit.title}));
 
+        //Format prerequisites array
+        const formattedPrerequisites = prerequisites.map((prerequisite) => ({title:prerequisite.title}));
+
+        //Format course content data
+        const formattedCourseContentData = courseContentData.map((courseContent) => ({
+            videoUrl:courseContent.videoUrl,
+            title:courseContent.title,
+            description:courseContent.description,
+            videoSection:courseContent.videoSection,
+            links:courseContent.links.map((link) => ({
+                title:link.title,
+                url:link.url
+            })),
+            suggestion:courseContent.suggestion
+        }));
+
+        //prepare out data object
+        const data = {
+            name: courseInfo.name,
+            description: courseInfo.description,
+            price: courseInfo.price,
+            estimatedPrice: courseInfo.estimatedPrice,
+            tags: courseInfo.tags,
+            thumbnail: courseInfo.thumbnail,
+            level: courseInfo.level,
+            demoUrl: courseInfo.demoUrl,
+            totalVideos: courseContentData.length,
+            benefits: formattedBenefits,
+            prerequisites: formattedPrerequisites,
+            courseContent: formattedCourseContentData,
+        };
+        setCourseData(data);
     }
+    console.log(courseData);
 
     return (
         <div className="w-full flex min-h-screen">
@@ -72,6 +108,18 @@ const CreateCourse = (props: Props) => {
                 {
                     active === 2 && (
                         <CourseContent
+                            active={active}
+                            setActive={setActive}
+                            courseContentData={courseContentData}
+                            setCourseContentData={setCourseContentData}
+                            handleSubmit={handleSubmit}
+                        />
+                    )
+                }
+
+                {
+                    active === 3 && (
+                        <CoursePreview
                             active={active}
                             setActive={setActive}
                             courseContentData={courseContentData}
